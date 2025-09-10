0e390a59434f35edcc7817d676dfe5cd5306baab
thomfilg <thomfilg@gmail.com>
2025-09-10 12:53:32 -0300

    feat: adding ai-scripts-check lib

---
+++ packages/ai-script-checks/eslint.config.js
@@ -0,0 +1,20 @@
+export default [
+  {
+    root: true,
+    parser: '@typescript-eslint/parser',
+    plugins: ['@typescript-eslint'],
+    extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+    parserOptions: {
+      ecmaVersion: 2022,
+      sourceType: 'module',
+    },
+    env: {
+      node: true,
+      es2022: true,
+    },
+    rules: {
+      '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+      '@typescript-eslint/no-explicit-any': 'warn',
+    },
+  },
+];
